9f692fdf19d8f595db09fbb1865cf2d015651f72
SandraS2611 <sssandra1980@gmail.com>
2023-11-16 14:43:19 -0300

    practica del servidor

--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-// console.log("1) hola");
+const http = require("node:http")
 
-// setTimeout(() => {
-//     console.log("2) hola");
-// }, 5000)
-
-// console.log("3) hola");
-// console.log("4) hola");
-
-const miPromesa = new Promise((res, rej) => {
- setTimeout(()=> {
-   rej("hubo un error, no me devolvió la plata")
- //res("me devolvió la plata")
-}, 200)
+const servidor = http.createServer((req, res) => {
+res.end("Hola desde mi servidor http")
 })
 
-miPromesa
-.then((info) => {
-    console.log("then");
-    console.log(info);
-})
-.catch((error) => {
-    console.log("catch");
-    console.log(error);
-})
-.finally(() => {
-    console.log("si o si");
-})
+servidor.listen(3000)
+console.log("Server listening on Port 3000");
