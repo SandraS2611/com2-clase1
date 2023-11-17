d17dc30c10b209a3fc43531db77ed2fa7f6fe1e6
SandraS2611 <sssandra1980@gmail.com>
2023-11-16 23:31:53 -0300

    controladores

--- app.js
+++ app.js
@@ -8,19 +8,34 @@
 
 // //IP local = 127.0.0.1 === localhost :3000 === Puerto
 
-
 // app.listen(3000)
 // console.log("Server listening on Port 3000");
 
-import express from "express"
+import express from "express";
+import { ctrlGetAllPosts, ctrlCreatePosts } from "./mis-funciones.js";
 
-const app = express()
+const app = express();
 
-app.listen(3000)
+app.get("/posts", ctrlGetAllPosts);
+app.post("/posts", ctrlCreatePosts);
+
+app.patch("/posts", (req, res) => {
+  res.status(200).send("Hello from PATCH verb");
+});
+
+app.put("/posts", (req, res) => {
+  res.status(200).send("Hello from PUT verb");
+});
+
+app.delete("/posts", (req, res) => {
+  res.status(200).send("Hello from DELETE verb");
+});
+
+app.listen(3000);
 console.log("server on port 3000");
 
 //GET --> obtener recursos
 //POST --> crear recursos
 //PATCH --> editar recursos ya creados de forma parcial
 //DELETE --> eliminar recursos
-//PUT --> editar un recursos si existe y si no lo crea. 
+//PUT --> editar un recursos si existe y si no lo crea.
---
+++ mis-funciones.js
@@ -0,0 +1,10 @@
+export const ctrlGetAllPosts = (req, res) => {
+    res.status(200).send("Hello from GET verb");
+}
+
+export const ctrlCreatePosts = (req, res) => {
+    res.status(200).json
+}
+
+//export default controlador
+//export { controlador }
