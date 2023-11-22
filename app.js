c33712d0df8a6569fc8a7b1e3069fc365ac76565
SandraS2611 <sssandra1980@gmail.com>
2023-11-22 20:30:26 -0300

    install nodemon express

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ import express from "express"
 
 const app = express()
 
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
+app.post('/', (req, res) => {
+    res.send("Hello World from POST")
+})
+
+
 app.listen(3000)
 console.log("Server listening on port 3000");
-
-
-
-
-
-
-// app.get('/', (req, res) => {
-//     res.send("Hello World")
-// })
 
 
 // 127.0.0.1:3000  ===  localhost:3000
