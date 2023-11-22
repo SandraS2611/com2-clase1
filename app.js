ec4f121487cfcaa940f2e10cb3ad7ef80772a216
SandraS2611 <sssandra1980@gmail.com>
2023-11-22 20:09:51 -0300

    cambios por error anterior en todo

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
-// const express = require("express")
+import express from "express"
 
-// const app = express()
+const app = express()
 
-// app.get('/',(req, res) =>{
-//     res.send("hello world")
+app.listen(3000)
+console.log("Server listening on port 3000");
+
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World")
 // })
 
-// //IP local = 127.0.0.1 === localhost :3000 === Puerto
 
-// app.listen(3000)
-// console.log("Server listening on Port 3000");
-
-import express from "express";
-import { ctrlGetAllPosts, ctrlCreatePosts } from "./mis-funciones.js";
-
-const app = express();
-
-app.get("/posts", ctrlGetAllPosts);
-app.post("/posts", ctrlCreatePosts);
-
-app.patch("/posts", (req, res) => {
-  res.status(200).send("Hello from PATCH verb");
-});
-
-app.put("/posts", (req, res) => {
-  res.status(200).send("Hello from PUT verb");
-});
-
-app.delete("/posts", (req, res) => {
-  res.status(200).send("Hello from DELETE verb");
-});
-
-app.listen(3000);
-console.log("server on port 3000");
-
-//GET --> obtener recursos
-//POST --> crear recursos
-//PATCH --> editar recursos ya creados de forma parcial
-//DELETE --> eliminar recursos
-//PUT --> editar un recursos si existe y si no lo crea.
+// 127.0.0.1:3000  ===  localhost:3000
+// http://localhost:3000/
